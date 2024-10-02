c47474ec89862c6200d86d6a8073abe86e185109
josePereiro <jose.pereiro.personal@gmail.com>
2024-10-02 00:01:41 -0400

    WIP baldazziResourceAllocationAccounts2023

--- src/MetXCultureHub.jl
+++ src/MetXCultureHub.jl
@@ -2,6 +2,25 @@
 # Maybe use a custom Dict type for more searching/accessing capabilities
 # Create an interface where each data point is correctly defined/identify
 
+# NOTE: Do not use a custom system
+# - Think about a language agnostic simple database
+#   - ex: csv, sbml, json
+#   - Pick one and move on, once the data is digital it can be converted around.
+# - given the unstructure nature of culture data the format needs to be flexible
+#   - A first approch can be that a each papers data sets are in principle independent
+#       - That is, no interoperativity/consistency is required
+#       - But, the format need to be automatizable. 
+#   - Each dataset will have mod phases
+#       - raw: literally a transcription of the papers data
+#           - same ids, same units, all
+#           - this is the core of the package 
+#               - any post-processing of the data is just a pluggin
+#       - stdmod: standard default set of modifications for inter-dataset homogeneity
+#           - ej: Convert to common units
+
+# TODO: add: baldazziResourceAllocationAccounts2023
+# - this is almost done (see data/raw/baldazziResourceAllocationAccounts2023)
+
 # TODO: add: ihssenSpecificGrowthRate2004 (see folsomPhysiologicalBiomassElemental2015 supp 4)
 # TODO: add: huaAnalysisGeneExpression2004 (see folsomPhysiologicalBiomassElemental2015 supp 4)
 
